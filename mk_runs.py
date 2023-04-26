296c50291b3fafc4c8ba36181a7e1069a259287c
Peter Teuben <teuben@gmail.com>
2023-04-26 09:15:12 -0400

    new apr 26 data

--- mk_runs.py
+++ mk_runs.py
@@ -1,32 +1,26 @@
 #! /usr/bin/env python
 #
-#   script generator for project="2021-S1-US-3"
 #
-#   lmtinfo.py grep US-3 Science Map | awk '{print $2}' | sort
-
 
 import os
 import sys
 
-# in prep of the new lmtoy module
-try:
-    lmtoy = os.environ['LMTOY']
-    sys.path.append(lmtoy)
-    from lmtoy import runs
-except:
-    print("No LMTOY with runs.py")
-    sys.exit(0)
+from lmtoy import runs
 
 project="2023-S1-US-17"
 
 on = {}
 
+on["NGC6232"] =  [ 108853, 108854, 108855, 108857, 108858, 108859,]    # 25 apr
+
 on["VIIZw800"] = [ 107356, 107357, 107358, 107361, 107362, 107363,]    # 23/24 mar
 
 pars1 = {}
+pars1["NGC6232"]  = ""
 pars1["VIIZw800"] = "speczoom=104,3"
 
 pars2 = {}
+pars2["NGC6232"]  = "srdp=1 admit=0"
 pars2["VIIZw800"] = "srdp=1 admit=0"
 
 if __name__ == '__main__':    
