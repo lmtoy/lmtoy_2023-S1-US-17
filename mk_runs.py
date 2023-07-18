b565d95d37c40d21347681f366cb6ba9892137c8
LMT Helpdesk <mwatstein@umass.edu>
2023-07-18 18:41:22 +0000

    modified:   comments.txt 	modified:   mk_runs.py

--- mk_runs.py
+++ mk_runs.py
@@ -16,8 +16,8 @@ on["NGC6232"] =  [ 108853, 108854, 108855, 108857, 108858, 108859,]    # 25 apr
 on["VIIZw800"] = [ 107356, 107357, 107358, 107361, 107362, 107363,]    # 23/24 mar
 
 pars1 = {}
-pars1["NGC6232"]  = ""
-pars1["VIIZw800"] = "speczoom=104,3"
+pars1["NGC6232"]  = "badcb=3/3"
+pars1["VIIZw800"] = "speczoom=104,3 badcb=3/3"
 
 pars2 = {}
 pars2["NGC6232"]  = "srdp=1 admit=0"
